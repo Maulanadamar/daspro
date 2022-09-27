f1cfc4833340e08b9c993507520eeaa691f217fc
Maulanadamar <113022357+Maulanadamar@users.noreply.github.com>
2022-09-27 13:02:57 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+
+    /*nama:maulana damar adhesyah putra
+  kelompok :4116
+  NIM  :A11.2022.14620
+  */
+int panjang,lebar,tinggi,luas permukaan;
+//input
+cout  <<"masukan panjang : ";
+cin   >> p;
+cout  <<"masukan lebar: ";
+cin   >> l;
+cout  <<"masukan tinggi: ";
+cin   >> t;
+
+//hitung rumus luas permukaan balok
+luas permukaan = 2*(p*l+p*t+l*t);
+cout <<"panjang :"<<p<<endl;
+cout <<"lebar :"<<l<<endl;
+cout <<"tinggi :"<<t<<endl<<endl<<endl;
+cout <<"luas permukaan balok :"<<luas;
+
+    return 0;
+}
